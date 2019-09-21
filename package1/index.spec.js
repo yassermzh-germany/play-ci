6efcb6732b2b2031a32baf15a750430dd0cac080
Yasser Zadeh <yassermzh.germany@gmail.com>
2019-09-21 22:49:41 +0200

    add jasmine and tests

---
+++ package1/index.spec.js
@@ -0,0 +1,5 @@
+describe("packakge1", () => {
+    it("should pass", () => {
+        expect(1 + 1).toBe(2);
+    });
+});
---
+++ package2/index.spec.js
@@ -0,0 +1,5 @@
+describe("packakge2", () => {
+  it("should pass", () => {
+      expect(1 + 1).toBe(2);
+  });
+});
