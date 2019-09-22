aa9f92cac9846ae4d03e7da001082bd23fafaaf8
Yasser Zadeh <yassermzh.germany@gmail.com>
2019-09-22 21:08:59 +0200

    try again

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,11 +1,7 @@
 packages=$(./scripts/detect-changed-packages.sh)
 echo "packages=$packages"
-set -o pipefail
+# set -o pipefail
 PACKAGE="$packages" node jasmine.js | tee ./logs
-if [ $? -eq 0 ]
-then
-  exit 0
-else
-  cat ./logs | grep "No specs defined" -q && exit 0
-  exit 1
-fi
+grep -q Failed ./logs && exit 1
+# grep "No specs defined" -q ./logs && exit 0
+exit 0
